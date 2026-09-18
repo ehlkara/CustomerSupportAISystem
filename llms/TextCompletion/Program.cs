98ffb8900bf409d3830adced7204c03ad3ec5894
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2026-09-18 11:57:05 +0300

    created chatCompletion

--- llms/TextCompletion/Program.cs
+++ llms/TextCompletion/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+using System.ClientModel;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("http://localhost:11434/v1/")
+};
+
+IChatClient chatClient = new OpenAIClient(new ApiKeyCredential("ollama"), options).GetChatClient("llama3.1:8b").AsIChatClient();
+
+
+ChatResponse response = await chatClient.GetResponseAsync("What is AI ? explain max 20 words");
+
+Console.WriteLine(response);
